efdf2914b4057b37349878992ac4629717876ec4
manav <mdoshi02@students.cs.ubc.ca>
2023-11-19 11:55:23 -0800

    commit 1

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const toggleSpecify = (element) => {
+const specifyDiv = document.getElementById("accessibility-specify");
+
+if (element.id === "accessibility-yes") {
+    specifyDiv.style.display = "block";
+    document.getElementById("specify").required = true; // make the input required
+} else {
+    specifyDiv.style.display = "none";
+    document.getElementById("specify").required = false; // remove required attribute
+}
+}
