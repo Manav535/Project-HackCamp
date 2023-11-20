e0e27a778e0bcd152622a5261e6593b7be12649d
manav <mdoshi02@students.cs.ubc.ca>
2023-11-19 18:48:22 -0800

    final version

--- scriptTherapistPreferences.js
+++ scriptTherapistPreferences.js
@@ -45,3 +45,48 @@ document.getElementById("reset").addEventListener("click", () => {
     document.getElementById("diversity-specify").value = ""; // Clear input value if hidden
 })
     
+
+// Sample therapist data (Replace this with your therapist data)
+const therapists = [
+    { "name": "Jane Doe", "age": 38, "gender": "Female" },
+    { "name": "John Doe", "age": 45, "gender": "Male" },
+    // Add more therapists as needed
+];
+
+document.getElementById("survey-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    // Fetch the entered minimum age from the form
+    var givenAge = document.getElementById("age").value.trim(); // Get age input
+
+    // Fetch the selected gender from the form
+    var selectedGender = document.getElementById("gender").value.trim().toLowerCase(); // Get gender input
+
+    // Filter therapists based on the entered age and selected gender
+    var filteredTherapists = filterTherapistsByAgeAndGender(therapists, givenAge, selectedGender);
+
+
+
+    // Store filtered therapists in sessionStorage to access them on the next page
+    sessionStorage.setItem('filteredTherapists', JSON.stringify(filteredTherapists));
+
+    // Redirect to a new page to display the filtered therapists
+    window.location.href = 'filteredTherapists.html';
+});
+
+// Function to filter therapists by age and gender
+function filterTherapistsByAgeAndGender(therapists, givenAge, selectedGender) {
+    return therapists.filter(therapist => {
+        console.log(givenAge === '', selectedGender === '')
+        const ageMatch = givenAge === '' || (therapist.age >= parseInt(givenAge) - 5 && therapist.age <= parseInt(givenAge) + 5);
+        const genderMatch = selectedGender === "none" || therapist.gender.toLowerCase() === selectedGender;
+        console.log(ageMatch, genderMatch)
+        return ageMatch && genderMatch;
+    });
+}
+
+
+
+
+
+
